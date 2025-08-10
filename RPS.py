f4b00742c9fea27e869d9ea4ac109f2b3f4deb93
SARATH CHANDRA GANDI <sarathchandragandi@SARATHs-MacBook-Pro.local>
2025-08-10 06:11:49 +0530

    Added Rock Paper Scissors game

---
+++ RPS.py
@@ -0,0 +1,53 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+print("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors")
+user_choice = int(input())
+if user_choice == 0:
+    print(rock)
+elif user_choice == 1:
+    print(paper)
+elif user_choice == 2:
+    print(scissors)
+else:
+    print("Invalid choice. Please choose 0, 1, or 2.")
+    exit()
+computer_choice = random.randint(0,2)
+if computer_choice == 0:
+    print("Computer chose:\n" + rock)
+elif computer_choice == 1:
+    print("Computer chose:\n" + paper)
+elif computer_choice == 2:
+    print("Computer chose:\n" + scissors)
+if user_choice == computer_choice:
+    print("It's a draw!")
+elif (user_choice == 0 and computer_choice == 2) or \
+     (user_choice == 1 and computer_choice == 0) or \
+     (user_choice == 2 and computer_choice == 1):
+    print("You win!")
+else:
+    print("You lose!")
